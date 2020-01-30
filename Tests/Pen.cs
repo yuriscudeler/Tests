1b140a2aed9ce8cbe31d0d61184b36ed32d5d193
Yuri <yuriscudeler@gmail.com>
2020-01-30 14:06:35 +0000

    Initial commit

---
+++ Tests/Pen.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Tests
+{
+    public class Pen : IXmlSerializable
+    {
+        public string color;
+        public float price;
+        public string brand;
+
+        public override string ToString()
+        {
+            return string.Format("A {0} pen, which costs {1} and was made by {2}.", color, price, brand);
+        }
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            color = reader.GetAttribute("color");
+            price = float.Parse(reader.GetAttribute("price"));
+            brand = reader.GetAttribute("brand");
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteAttributeString("color", color);
+            writer.WriteAttributeString("price", price.ToString());
+            writer.WriteAttributeString("brand", brand);
+        }
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serialization();
+            Console.ReadKey();
+        }
+
+        static void Serialization()
+        {
+            Pen pen = new Pen()
+            {
+                color = "blue",
+                price = 1.0f,
+                brand = "Bic"
+            };
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Pen));
+            using (TextWriter writer = new StreamWriter("pen.xml"))
+            {
+                serializer.Serialize(writer, pen);
+            }
+
+            using (Stream fs = new FileStream("pen.xml", FileMode.Open))
+            {
+                Pen theSamePen = (Pen)serializer.Deserialize(fs);
+                Console.WriteLine(theSamePen.ToString());
+            }
+        }
+    }
+}
